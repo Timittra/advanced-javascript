c8f4e7ec19f895a2a762b9e062cdfd414bc6e398
Timittra Islam Hridi <timittra.cse.aust@gmail.com>
2021-02-02 03:29:42 +0600

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,24 @@
+let pakhi;
+do not return any value from a function
+write only return without anything
+
+function add(num1, num2){
+    console.log(num1+num2);
+    return 
+}
+
+
+function add(num1, num2){
+    console.log(num1, num2);
+  
+}
+const result = add(50);
+
+const premik = {name: 'smart dude', age:21};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [12, 15, 18];
+console.log(ages[19]);
