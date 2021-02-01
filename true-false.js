e1b25f3f5ecd6e42d1582ecab2819d223ce739a9
Timittra Islam Hridi <timittra.cse.aust@gmail.com>
2021-02-02 03:04:26 +0600

    true false

---
+++ true-false.js
@@ -0,0 +1,13 @@
+// const age =0; return false
+// const named = ''; false
+// let names; undefined and false
+// let names = null; false
+// let names = NaN; false
+// "0" " " [] {} true
+const named = 0;
+if(named || named == 0){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is not true");
+}
